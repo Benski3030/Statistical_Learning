92eba6699b637b86191d27cbc744c74587e3a2d0
Ben <benski3030#@gmail.com>
2016-04-11 11:17:05 -0500

    Update p_value_tests.R
    Code error calling qplot on line 10

--- Frequentist/p_value_tests.R
+++ Frequentist/p_value_tests.R
@@ -7,23 +7,24 @@ theme_set(theme_bw())
 
 set.seed(132533)
 our_population <- rnorm(10000, 15, 2.5)
-qplot(our_data)
+qplot(our_population)
 
 descdist(our_population, discrete = F, boot = 500)
 
 psampler <- function (x, boot = 500, mean = 0, test_size = 100) {
-    pvals <- c()
-    bootstrap <- seq(0, boot)
-    for (i in 1:length(bootstrap)) {
-      rand_sample <- sample(x, size = test_size, replace = FALSE)
-      test <- t.test(rand_sample, mu = mean)
-      pval <- test$p.value
-      pvals <- append(pvals, pval)
-    }
-    df <- data.frame(samp_num = bootstrap, p_val = pvals)
-    psub <- subset(pvals, pvals > 0.05)
-    print(paste("The bootstrapped false positive rate is:",round(length(psub)/length(bootstrap),3)*100,"%"))
-    return(df)
+  pvals <- c()
+  bootstrap <- seq(0, boot)
+  for (i in 1:length(bootstrap)) {
+    rand_sample <- sample(x, size = test_size, replace = FALSE)
+    test <- t.test(rand_sample, mu = mean)
+    pval <- test$p.value
+    pvals <- append(pvals, pval)
+  }
+  df <- data.frame(samp_num = bootstrap, p_val = pvals)
+  psub <- subset(pvals, pvals > 0.05)
+  print(paste("The bootstrapped false positive rate is:",round(length(psub)/length(bootstrap),5)*100,"%"))
+  return(df)
 }
 
-pvals <- psampler(our_population, boot = 1000, mean = 14.25, test_size = 1000)
+pvals <- psampler(our_population, boot = 1000, mean = 14, test_size = 60)
+qplot(pvals$p_val)
