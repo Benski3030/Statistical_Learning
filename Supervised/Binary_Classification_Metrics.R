d887198ffc67c1e2423d49908fa07fda3443b3c9
Ben <benski3030#@gmail.com>
2016-04-18 16:00:11 -0500

    Update Binary_Classification_Metrics.R

--- Supervised/Binary_Classification_Metrics.R
+++ Supervised/Binary_Classification_Metrics.R
@@ -10,7 +10,7 @@ model_diagnostics <- function (predicted, actual, model_name = "Unkown Classifie
   cat("Model Diagnostics:", model_name, "\n") #prints out the model name
   print(my_tbl)
   accuracy <-  sum(diag) / n  #Accuracy measurement
-  cat("\n","The overall accuracy of the predictions is:", accuracy * 100,"%", "\n") #pretty print accuracy
+  cat("The overall accuracy of the predictions is:", accuracy * 100,"%", "\n") #pretty print accuracy
   precision <-  diag / colsums #calculate precision
   recall <-  diag / rowsums  #calculate recall
   oneVsAll <-  lapply(1 : nc, #one v. all approximation
@@ -23,7 +23,7 @@ model_diagnostics <- function (predicted, actual, model_name = "Unkown Classifie
   s <-  matrix(0, nrow <-  2, ncol <-  2)
   for(i in 1 : nc){s <-  s + oneVsAll[[i]]} #for loop for list index
   avgAccuracy <-  sum(diag(s)) / sum(s) #calucate the average accuracy
-  cat("\n","The average accuracy of the predictions is:", avgAccuracy * 100,"%", "\n") 
+  cat("The average accuracy of the predictions is:", avgAccuracy * 100,"%", "\n") 
   f1 <-  2 * precision * recall / (precision + recall) 
   cat("\n","The balance of Precision v. Recall is:","\n")
   print(data.frame(precision, recall, f1)) #summary stats
@@ -33,7 +33,7 @@ model_diagnostics <- function (predicted, actual, model_name = "Unkown Classifie
   kappa <-  (accuracy - expAccuracy) / (1 - expAccuracy) #kappa statistic
   cat("\n","The kappa statistic is", kappa, ", Which is the measure of agreement between the predictions and actual labes, 0 is bad, 1 is good")
   if (accuracy > 0.8 & avgAccuracy > 0.8 & kappa > 0.8) { #quick guy check
-    cat("\n","This appears to be a good model")
+    cat("\n","This appears to be a good model","\n")
   } else {
     cat("\n","Something isn't right, check the model again")
   }
