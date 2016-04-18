27ba4ad01c12c2e4053eaa269273da9ced5ab7b6
Ben <benski3030#@gmail.com>
2016-04-18 15:57:56 -0500

    Update Binary_Classification_Metrics.R

--- Supervised/Binary_Classification_Metrics.R
+++ Supervised/Binary_Classification_Metrics.R
@@ -7,10 +7,10 @@ model_diagnostics <- function (predicted, actual, model_name = "Unkown Classifie
   colsums <- apply(my_tbl, 2, sum) # number of predictions per class
   p <- rowsums / n #instances over the actual classes
   q <- colsums / n #instances over the predicted classes
-  print(paste("Model Diagnostics:", model_name)) #prints out the model name
+  cat("Model Diagnostics:", model_name, "\n") #prints out the model name
   print(my_tbl)
   accuracy <-  sum(diag) / n  #Accuracy measurement
-  print(paste("The overall accuracy of the predictions is:", accuracy * 100,"%")) #pretty print accuracy
+  cat("\n","The overall accuracy of the predictions is:", accuracy * 100,"%", "\n") #pretty print accuracy
   precision <-  diag / colsums #calculate precision
   recall <-  diag / rowsums  #calculate recall
   oneVsAll <-  lapply(1 : nc, #one v. all approximation
@@ -23,18 +23,18 @@ model_diagnostics <- function (predicted, actual, model_name = "Unkown Classifie
   s <-  matrix(0, nrow <-  2, ncol <-  2)
   for(i in 1 : nc){s <-  s + oneVsAll[[i]]} #for loop for list index
   avgAccuracy <-  sum(diag(s)) / sum(s) #calucate the average accuracy
-  print(paste("The average accuracy of the predictions is:", avgAccuracy * 100,"%")) 
+  cat("\n","The average accuracy of the predictions is:", avgAccuracy * 100,"%", "\n") 
   f1 <-  2 * precision * recall / (precision + recall) 
-  print("The balance of Precision v. Recall is:")
+  cat("\n","The balance of Precision v. Recall is:","\n")
   print(data.frame(precision, recall, f1)) #summary stats
-  print("Precision is defined as: Out of the items that the classifier predicted to be correct, how many are truly correct?")
-  print("Recall is defined as: Out of all the items that are truly correct, how many were found by the classifier?")
+  cat("\n","Precision is defined as: Out of the items that the classifier predicted to be correct, how many are truly correct?")
+  cat("\n","Recall is defined as: Out of all the items that are truly correct, how many were found by the classifier?","\n")
   expAccuracy <-  sum(p*q) #expected accuracy
   kappa <-  (accuracy - expAccuracy) / (1 - expAccuracy) #kappa statistic
-  print(paste("The kappa statistic is", kappa, ", Which is the measure of agreement between the predictions and actual labes, 0 is bad, 1 is good"))
+  cat("\n","The kappa statistic is", kappa, ", Which is the measure of agreement between the predictions and actual labes, 0 is bad, 1 is good")
   if (accuracy > 0.8 & avgAccuracy > 0.8 & kappa > 0.8) { #quick guy check
-    print("This appears to be a good model")
+    cat("\n","This appears to be a good model")
   } else {
-    print("Something isn't right, check the model again")
+    cat("\n","Something isn't right, check the model again")
   }
 }
