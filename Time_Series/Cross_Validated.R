1939db5a2c0c546f90f762e276fa39edaa81e7a4
Ben <benski3030#@gmail.com>
2017-05-03 10:38:26 -0500

    Update Cross_Validated.R

--- Time_Series/Cross_Validated.R
+++ Time_Series/Cross_Validated.R
@@ -1,6 +1,5 @@
 library(cv.ts)
 require(reshape2)
-require(ggplot2)
 library(forecast)
 library(foreach)
 library(tseries)
@@ -32,27 +31,39 @@ myControl <- tseriesControl(
 )
 
 #Forecast using several models
+result_ets <- cv.ts(fcastts, etsForecast, myControl,ic='bic')
+#result_caret <- cv.ts(fcastts, caretForecast, myControl)
+result_mean <- cv.ts(fcastts, meanForecast, myControl)
+result_lm <- cv.ts(fcastts, lmForecast, myControl)
 result_naive <- cv.ts(fcastts, snaiveForecast,myControl)
 result_autoarima <- cv.ts(fcastts, auto.arimaForecast, myControl,ic='bic')
-result_ets <- cv.ts(fcastts, etsForecast, myControl,ic='bic')
-result_theta <- cv.ts(fcastts, thetaForecast, myControl)
-result_lm <- cv.ts(fcastts, lmForecast, myControl)
+result_sts <- cv.ts(fcastts, stsForecast, myControl)
 result_tbats <- cv.ts(fcastts, tbatsForecast, myControl)
+result_nn <- cv.ts(fcastts, nnetarForecast, myControl)
+result_rw <- cv.ts(fcastts, rwForecast, myControl)
+result_theta <- cv.ts(fcastts, thetaForecast, myControl)
 
-#Plot error
+
 plotData <- data.frame(
-  horizon=1:12
-  ,naive =result_naive$results$MAPE[1:12]
-  ,arima=result_autoarima$results$MAPE[1:12]
-  ,ets=result_ets$results$MAPE[1:12]
-  ,theta=result_theta$results$MAPE[1:12]
-  ,lm=result_lm$results$MAPE[1:12]
-  ,tbats=result_tbats$results$MAPE[1:12]
+  horizon=1:12,
+  ets = result_ets$results$MAPE[1:12],
+  mean = result_mean$results$MAPE[1:12],
+  lm = result_lm$results$MAPE[1:12],
+  naive = result_naive$results$MAPE[1:12],
+  arima_aa = result_autoarima$results$MAPE[1:12],
+  sts = result_sts$results$MAPE[1:12],
+  tbats = result_tbats$results$MAPE[1:12],
+  nn = result_nn$results$MAPE[1:12],
+  rw = result_rw$results$MAPE[1:12],
+  theta = result_theta$results$MAPE[1:12]
 )
+
+
 plotData <- melt(plotData, id.vars='horizon', value.name='MAPE', variable.name='model')
 ggplot(plotData, aes(horizon, MAPE, color=model)) + geom_line() + 
-  theme_bw()
+  theme_bw() + 
+  ggtitle("Forecast Cross Validation Performance Results")
 
 #choose the best fit and forecast away
-fit <- tbats(fcastts)
+fit <- thetaf(fcastts)
 plot(forecast(fit, h=12))
